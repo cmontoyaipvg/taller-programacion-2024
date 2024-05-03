8a2620ab62c004b451bd05cf7bfdcd81f68e6d07
claudio montoya <claudio.montoya@biopc.cl>
2024-05-03 19:44:11 -0400

    se crea proyecto de listas

---
+++ listas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //es este ejemplo se usaran las listas dictiory y arraylist
+
+
+        }
+    }
+}
