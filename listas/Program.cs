30882a4bfb640b6f4f3c8c10ec33c28b13a1c4ab
claudio montoya <claudio.montoya@biopc.cl>
2024-05-10 19:11:13 -0400

    proyecto de listas

--- listas/Program.cs
+++ listas/Program.cs
@@ -12,8 +12,46 @@ namespace listas
         {
             //es este ejemplo se usaran las listas dictiory y arraylist
             // este es un nuevo comentario
+           /* int num1 = 10;
+            int num2 = 20;
+            Console.WriteLine($"La suma es {sumar(num1,num2)}");
+            int resultado = sumar(num1);
+            saludar("Claudio");
+           */
+            int valor = 10;
+            Console.WriteLine("1.- "+valor);
+            Sumar10(ref valor);
+            //Console.WriteLine("R:"+Sumar10(valor));
+            Console.WriteLine("2.- "+valor);
 
+            Console.ReadKey();
 
+        }
+
+        static int sumar(int x,int y = 20)
+        {
+            return x + y;
+        }
+        static void saludar(string nombre){
+            Console.WriteLine($"Bienvenido a este programa {nombre}");
+        }
+
+        static bool comparacion(int n1, int n2, int n3)
+        {
+          return (n1 == n2 && n2 == n3 && n1 ==n3);
+        }
+        static bool comparacion(int n1, int n2)
+        {
+            return (n1 == n2);
+        }
+        static bool comparacion(string n1, string n2)
+        {
+            return (n1 == n2);
+        }
+        static int Sumar10(ref int valor)
+        {
+            valor = valor + 10;
+            return valor;
         }
     }
 }
---
+++ listas2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace listas2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // uso de listas 
+            //declaracion
+            List<string> frutas = new List<string>();
+            List<int> numeros = new List<int>();
+            List<string> nombre = new List<string>() {"juan","pedro","diego","ana","lucia" };
+            List<string> rut = new List<string>() { "15213084-8", "14521455-K" };
+            Dictionary<string,string> personas = new Dictionary<string,string>();
+
+            personas["15213084-8"] = "Claudio Montoya";
+
+            Console.WriteLine(personas["15213084-8"]);
+
+            /*
+            rut.Add("11587963-8");
+            rut.Add("12587568-4");
+            */
+            nombre.Sort((a, b) => a.CompareTo(b));
+
+            imprimirLista(nombre);
+
+            //bool encontrar = rut.Contains("14521455-K");
+           // Console.WriteLine(encontrar);
+            
+            
+            //agregar elementos
+
+           /* frutas.Add("manzana");
+            frutas.Add("pera");
+            frutas.Add("uva");
+            frutas.Add("frutilla");
+            frutas.Add("platano");
+            frutas.Add("kiwi");
+
+
+
+            numeros.Add(1);
+            numeros.Add(2);
+            numeros.Add(3);
+
+            Console.WriteLine("elementos:"+frutas.Count());
+            Console.WriteLine(frutas.IndexOf("platano"));
+            Console.WriteLine("El platano esta en la posicion 4 :"+ frutas[4]);
+            
+            frutas.RemoveAt(2);
+            
+            imprimirLista(frutas);
+            imprimirLista(numeros);
+
+            frutas.Clear();
+            Console.WriteLine("elementos:" + frutas.Count());
+           */
+            Console.WriteLine("fin del programa");
+            Console.ReadKey();
+        }
+
+        static void imprimirLista(List<string> lista) {
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void imprimirLista(List<int> lista)
+        {
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
